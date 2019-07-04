b602b04b8765856fd03cfa35028ded4f532094c6
nuomi1 <nuomi1@qq.com>
2019-07-04 21:17:26 +0800

    Initial Commit

---
+++ Swift-Utils/Swift_Utils.h
@@ -0,0 +1,19 @@
+//
+//  Swift_Utils.h
+//  Swift-Utils
+//
+//  Created by nuomi on 4/7/2019.
+//  Copyright © 2019 nuomi1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swift_Utils.
+FOUNDATION_EXPORT double Swift_UtilsVersionNumber;
+
+//! Project version string for Swift_Utils.
+FOUNDATION_EXPORT const unsigned char Swift_UtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Utils/PublicHeader.h>
+
+
